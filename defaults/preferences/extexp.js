8ecef09f2658d7d0c70aae6f104c8682618fd1e2
Andy <webmaster@RealityRipple.com>
2018-01-16 21:29:32 -0800

    +New GUID.

--- defaults/preferences/extexp.js
+++ defaults/preferences/extexp.js
@@ -1,1 +1,1 @@
-pref("extensions.{4CAFE6F1-50EA-44FE-8E50-31C6B21E8032}.description", "chrome://extexp/locale/extexp.properties");
+pref("extensions.{A1B40EBE-CFA1-5353-8C69-9B76E30B8788}.description", "chrome://extexp/locale/extexp.properties");
